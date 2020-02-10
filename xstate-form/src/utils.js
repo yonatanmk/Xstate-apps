45e2fb91584e2e3a5c341da8e592fc192bde5f9c
yonatanmk <yonatanmk@gmail.com>
2020-02-10 17:22:40 -0500

    Xstate-form

---
+++ xstate-form/src/utils.js
@@ -0,0 +1,19 @@
+export function validate(value) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return /^[a-zA-Z]+$/.test(value) ? resolve() : reject();
+    }, 1000);
+  });
+}
+
+export function submit(values) {
+  console.log("Submitting", values);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return Math.random() < 0.4 || values.one === "error"
+        ? reject()
+        : resolve();
+    }, 1000);
+  });
+}
