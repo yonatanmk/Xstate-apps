4a449c73d87e2a18387b4b77d9dde6bf8a3c522a
yonatanmk <yonatanmk@gmail.com>
2020-02-17 15:53:57 -0500

    functioning form

--- xstate-simple-form/src/App.js
+++ xstate-simple-form/src/App.js
@@ -1,24 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Machine, assign } from "xstate";
+import { useMachine } from "@xstate/react";
+
+const formMachine = Machine({
+  initial: "editing",
+  context: {
+    name: "",
+    email: "",
+    password: "",
+  },
+  states: {
+    editing: {
+      on: {
+        CHANGE: {
+          actions: assign({
+            name: (ctx, e) => e.name || ctx.name,
+            email: (ctx, e) => e.email || ctx.email,
+            password: (ctx, e) => e.password || ctx.password,
+          })
+        },
+        SUBMIT: "submitted"
+      },
+    },
+    submitted: {
+      type: "final"
+    },
+  },
+});
+
 
 function App() {
+  const [current, send] = useMachine(formMachine)
+  const { name, email, password } = current.context;
+
+  const invalid = false;
+  const submitted = current.matches("submitted");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          send("SUBMIT");
+        }}
+      >
+        <h3>Sign Up</h3>
+        {invalid ? <div className="error">Sorry, that's invalid.</div> : null}
+        <div className="formblock">
+          <label>Name</label>
+          <input
+            autoFocus
+            type="text"
+            value={name}
+            onChange={e => send("CHANGE", { name: e.target.value })}
+          />
+        </div>
+        <div className="formblock">
+          <label>Email</label>
+          <input
+            type="text"
+            value={email}
+            onChange={e => send("CHANGE", { email: e.target.value })}
+          />
+        </div>
+        <div className="formblock">
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={e => send("CHANGE", { password: e.target.value })}
+          />
+        </div>
+        {!submitted && <button data-testid="save-button">
+          Save
+        </button>}
+      </form>
+      {submitted && (
+        <div>
+          <p>Name: {name}</p>
+          <p>Email: {email}</p>
+          <p>Password: {password}</p>
+        </div>
+      )}
     </div>
   );
 }
